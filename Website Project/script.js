36bd15c5be7f5a74ff33271d9b935c0314b8a45a
zsaund3 <zachary.saunders@protonmail.com>
2025-02-14 22:16:17 -0500

    Delete Website Project directory

--- Website Project/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    var email = document.getElementById("email").value;
-    var confirmEmail = document.getElementById("confirmEmail").value;
-
-    if (email !== confirmEmail) {
-        alert("Emails do not match. Please try again.");
-        event.preventDefault();
-    }
-});
